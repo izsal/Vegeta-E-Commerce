8cffece8a64b6550d54cc14a2f43a1eb6b43f0c2
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-12-17 18:18:48 +0700

    feat: 🎸 Sign In With Next Auth
    Signin endpoint, validate password, install next-auth, sigin form
    integration

---
+++ src/app/api/auth/sign-in/route.ts
@@ -0,0 +1,38 @@
+import Response from "@/lib/api.response";
+import { prisma } from "@/lib/prisma";
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+export async function POST(req: Request) {
+  try {
+    const payload = await req.json();
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+
+    if (!user || !bcrypt.compareSync(payload.password, user.password)) {
+      return Response({
+        message: "Incorrect email and password",
+        status: 404,
+      });
+    }
+    const data: Partial<User> = {
+      ...user,
+      password: undefined,
+    };
+
+    return Response({
+      message: "Sign in Successfully",
+      data,
+    });
+  } catch (error) {
+    return Response({
+      message: "Sign in Failed",
+      data: error,
+      status: 500,
+    });
+  }
+}
